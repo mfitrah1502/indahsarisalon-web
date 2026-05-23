2947bc30e05cbbd8e5ab277e5118cd9ac163f25a
alterjoo <mfitrah15ofcc@gmail.com>
2026-05-23 14:36:03 +0700

    fixing otp

--- config/mail.php
+++ config/mail.php
@@ -47,6 +47,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'verify_peer' => env('MAIL_SSL_VERIFY_PEER', env('APP_ENV', 'production') !== 'local'),
         ],
 
         'ses' => [
